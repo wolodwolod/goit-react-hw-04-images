176a645c8acac9c5d17607f6d1bcf29d8ccf86c3
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-02-18 19:16:19 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import fetchData from 'api/pixabayApi';
