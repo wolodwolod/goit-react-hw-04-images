0f1a05e5efc79708025a258079e8284219b5f847
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-02-21 15:51:01 +0200

    21/02

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import  usePrevious  from 'helper/usePrevious';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import fetchData from 'api/pixabayApi';
+
 import  Container  from 'components/Container';
 import  Searchbar  from 'components/Searchbar';
 import  ImageGallery  from 'components/ImageGallery';
-// import  ImageGalleryItem  from 'components/ImageGalleryItem';
+
 import  Button  from 'components/Button';
 import { AfterButton } from 'components/Button/AfterButton';
 import  Modal  from 'components/Modal';
 import Loader from 'components/Loader';
-
-// import s from './App.module.css';
 
 
 const Status = {
@@ -36,26 +35,19 @@ function App() {
   const [query, setQuery] = useState('');
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
-  const [showModal, setShowModal] = useState(false);
-  const [largeImageURL, setLargeImageURL] = useState(null);
-  const [total, setTotal] = useState(0);
-  const [tags, setTags] = useState('');
-  const [status, setStatus] = useState(Status.IDLE);
+   const [total, setTotal] = useState(0);
+    const [status, setStatus] = useState(Status.IDLE);
 
-  // state = {
-  //   images: [],
-  //   query: '',
-  //   error: null,
-  //   page: 1,
-  //   showModal: false,
-  //   largeImageURL: null,
-  //   status: Status.IDLE,
-  // };
+    const [modal, setModal] = useState({
+        open: false,
+        largeImageURL: null,
+        tags: '',        
+    });
 
-  // Произошло обновление query или page? Запрос. 
-  //  const loadNextPage = (page !== prevState.page && page !== 1);
-  const prevSPage = usePrevious({ page });
-  const loadNextPage = (page !== prevSPage && page !== 1);
+
+    // Произошло обновление query или page? Запрос. 
+  const prevPage = usePrevious({ page });
+  const loadNextPage = (page !== prevPage && page !== 1);
   
   useEffect(() => {
     if (query === '') return;
@@ -67,29 +59,9 @@ function App() {
         
   }, [query, page]);
   
-  // useEffect(() => {
-  //   fetchData();
-  //   setStatus(Status.PENDING_MORE);
     
-  // }, [page]);
-  
-  // componentDidUpdate(prevProps, prevState) {
-  
-  //   const { images, query, page  } = this.state;
-  //   const loadNextPage = (page !== prevState.page && page !== 1);
-     
-  //   // Запуск функции - запроса
-  //   if (query !== prevState.query || loadNextPage) {
-           
-  //     if (loadNextPage) { this.setState({ status: Status.PENDING_MORE }) }
-  //     if (images.length === 0) { this.setState({ status: Status.PENDING }) }
-      
-  //     this.fetchImages();
-  //  }
-  // }
-
   // Функция - запрос
-  
+    
   const fetchImages = () => {
         
     fetchData(query, page, params)
@@ -100,6 +72,7 @@ function App() {
         const totalPages = Math.ceil(totalHits / params.PER_PAGE);
 
         if (!hits.length) {
+          setStatus(Status.IDLE)
           return toast.error('Sorry, no images found. Please, try again!');
         }
         if (page === 1) {
@@ -121,17 +94,11 @@ function App() {
         setImages([...images, ...newImages]);
         setTotal(totalHits);
         setStatus(Status.RESOLVED);
-        
-        // this.setState(({ images }) => ({
-        //   images: [...images, ...newImages],
-        //   total: totalHits,
-        //   status: Status.RESOLVED
-      })
+          })
       .catch(error =>
         setError(error),
         setStatus(Status.REJECTED))
   }
-
    
   // Методы
   
@@ -144,24 +111,31 @@ function App() {
     setError(null);
   };
 
-  const toggleModal = (largeImageURL, tags) => {
-    setShowModal(!showModal);
-    setLargeImageURL(largeImageURL);
-    setTags(tags);
-  };
-
-  const onLoadMore = () => {
+  const openModal = (largeImageURL, tags) => {
+    setModal({
+      open: true,
+      largeImageURL: largeImageURL,
+      tags: tags,
+    });
+  }
+    
+  const closeModal = (largeImageURL, tags) => {
+    setModal({
+      open: false,
+      largeImageURL: null,
+      tags: '',
+    });
+  }
+  
+    
+    const onLoadMore = () => {
     setPage(page => page + 1);
                };
   
 
   // Разметка
 
-  // render() {
-// const { images, error, showModal, largeImageURL, tags, total, status } =
-//       this.state;
-    
-    const loadMoreBtn =
+      const loadMoreBtn =
       status === 'resolved'
       && images.length !== 0
       && images.length !== total;
@@ -177,22 +151,22 @@ return (
 
     {status === 'idle' && <div style={{ margin: 'auto' }}>PLEASE, INPUT A QUERY ! </div>}
                      
-    {(status === 'resolved' || status === 'pending_more') && <ImageGallery images={images} onClick={toggleModal} />}
+    {(status === 'resolved' || status === 'pending_more') && <ImageGallery images={images} onClick={openModal} />}
     {loadMoreBtn && <Button onClick={onLoadMore}>Load more</Button>}
     {status === 'pending_more' && <AfterButton>Loading...</AfterButton>}
         
     {(status === 'pending' || status === 'pending_more') && <Loader />}
         
-    {showModal && (
-      <Modal onClose={toggleModal}>
-        <img src={largeImageURL} alt={tags} />
+    {modal.open && (
+      <Modal onClose={closeModal}>
+        <img src={modal.largeImageURL} alt={modal.tags} />
       </Modal>
     )}
             
     <ToastContainer theme="colored" position="top-right" autoClose={5000} />
   </Container>
 );
-    
+   
 
 }
 export default App;
